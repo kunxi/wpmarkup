b933ad85b0e8dbdc80c13d22b4abcf59516d2cf1
Kun Xi <kunxi@kunxi.org>
2010-07-21 16:24:21 -0700

    With wpautop, the render seems to do the right thing. We need to add a util helper function to extract the raw post data from exported WordPress xml file, and compare the rendered HTML with our implementation.

--- wpmarkup/__init__.py
+++ wpmarkup/__init__.py
@@ -12,7 +12,6 @@ class Markup:
     #translators: closing tokeny quote
     closing_quote = '&#8221;'
 
-            
     cockney = [ "'tain't","'twere","'twas","'tis","'twill","'til","'bout","'nuff","'round","'cause"]
     cockneyreplace = [ "&#8217;tain&#8217;t","&#8217;twere","&#8217;twas","&#8217;tis","&#8217;twill","&#8217;til","&#8217;bout","&#8217;nuff","&#8217;round", "&#8217;cause" ]
     static_characters = [ '---', ' -- ', '--', ' - ', 'xn&#8211;', '...', '``', '\'s', '\'\'', ' (tm)' ]
@@ -20,28 +19,57 @@ class Markup:
     static_dict = dict(zip(static_characters+cockney, static_replacements+cockneyreplace))
     static_regex = re.compile("(%s)" % "|".join(map(re.escape, static_dict.keys())))
 
-    dynamic_characters = [ "'(\d\d(?:&#8217;|')?s)", '(\s|\A|")\'', '(\d+)"', "(\d+)'", "(\S)'([^'\s])", '(\s|\A)"(?!\s)', '"(\s|\S|\Z)', "'([\s.]|\Z)", '(\d+)x(\d+)', "(<.*>|[.*])" ]
-    dynamic_replacements = [ r'&#8217;\1',r'\1&#8216;', r'\1&#8243;', r'\1&#8242;', r'\1&#8217;\2', r'\1%s' % opening_quote , r'%s\1' % closing_quote, r'&#8217;\1', r'\1&#215;\2', r'&#038;\1' ]
+    dynamic_characters = [ "'(\d\d(?:&#8217;|')?s)", '(\s|\A|")\'', '(\d+)"', "(\d+)'", "(\S)'([^'\s])", '(\s|\A)"(?!\s)', '"(\s|\S|\Z)', "'([\s.]|\Z)", '(\d+)x(\d+)', '&([^#])(?![a-zA-Z1-4]{1,8};)' ]
+    dynamic_replacements = [ '&#8217;\1', '\1&#8216;', '\1&#8243;', '\1&#8242;', '\1&#8217;\2', '\1%s' % opening_quote , '%s\1' % closing_quote, '&#8217;\1', '\1&#215;\2', '&#038;\1' ]
     dynamic_regex = zip([ re.compile(x, re.DOTALL) for x in dynamic_characters ], dynamic_replacements)
 
     no_texturize_tags = ['pre', 'code', 'kbd', 'style', 'script', 'tt']
     no_texturize_shortcodes = ['code']
     token_regex = re.compile('(<.*?>|\[.*?\])', re.DOTALL)
 
+    allblocks = '(?:table|thead|tfoot|caption|col|colgroup|tbody|tr|td|th|div|dl|dd|dt|ul|ol|li|pre|select|form|map|area|blockquote|address|math|style|input|p|h[1-6]|hr)'
+    multiline_regexs = [
+        [re.compile('<br />\s*?<br />', re.M), "\n\n"],
+        [re.compile('<%s[^>]*>' % allblocks), "\n\1"],
+        [re.compile('</%s\s*>' % allblocks), "\n\n\1"],
+        [re.compile('(\r\n|\r)', re.M), "\n"],
+        [re.compile('\s*<param([^>]*)>\s*', re.M), "<param\1>"],
+        [re.compile('\s*<embed>\s*', re.M), "<embed>"],
+        [re.compile('\n\n+', re.M), "\n\n" ],
+    ]
+    newline_split_regex = re.compile("\n\s*\n")
+    oneline_regexs = [
+        [re.compile("<p>\s*</p>"), ''],
+        [re.compile("<p>([^<]+)</(div|address|form)>"), '<p>\1</p></2>'],
+        [re.compile("<p>\s*(</?%s[^>]*>)\s*</p>" % allblocks), '\1'],
+        [re.compile("<p>(<li.+?)</p>"), '\1'],
+        [re.compile("<p><blockquote([^>]*)>", re.IGNORECASE), '<blockquote\1><p>'],
+        [re.compile("</blockquote></p>"), '</p></blockquote>'],
+        [re.compile("<p>\s*(</?%s'[^>]*>)" % allblocks), '\1'],
+        [re.compile("(</?%s[^>]*>)\s*</p>" % allblocks), '\1'],
+        # split to oneline2_regex if we want to support br=1
+        [re.compile("(</?%s[^>]*>)\s*<br />" % allblocks), '\1'],
+        [re.compile("<br />(\s*</?(?:p|li|div|dl|dd|dt|th|pre|td|ul|ol)[^>]*>)"), '\1'],
+        [re.compile("(<pre[^>]*>)(.*?)</pre>", re.I), 
+            lambda m: "%s%s</pre>" % match.groups().replace('<br />', '') \
+            .replace('<p>', '\n').replace('</p>', '') ],
+        [re.compile("\n</p>$"), '</p>'],
+    ]
+
     @staticmethod
     def render(raw):
-        """
-        >>> Markup.render(''''cause today's effort makes it worth tomorrow's "holiday"...''')
-        '&#8217;cause today&#8217;s effort makes it worth tomorrow&#8217;s &#8220;holiday&#8221;&#8230;'
-
-        >>> Markup.render('<pre>sadsadasd</code>"baba"</pre>')
-        '<pre>sadsadasd</code>"baba"</pre>'
-        """
-        return Markup.wptexturize(raw)
+        return Markup.wpautop(Markup.wptexturize(raw))
 
 
     @staticmethod
     def wptexturize(raw):
+        """
+        >>> Markup.wptexturize(''''cause today's effort makes it worth tomorrow's "holiday"...''')
+        '&#8217;cause today&#8217;s effort makes it worth tomorrow&#8217;s &#8220;holiday&#8221;&#8230;'
+
+        >>> Markup.wptexturize('<pre>sadsadasd</code>"baba"</pre>')
+        '<pre>sadsadasd</code>"baba"</pre>'
+        """
         no_texturize_tags_stack = []
         no_texturize_shortcodes_stack = []
         output = []
@@ -81,7 +109,29 @@ class Markup:
                 break
 
 
+    @staticmethod
+    def wpautop(pee, br = 1):
+        if pee.strip() == '':
+            return ''
+
+        pee += '\n'
+
+        for regex, repl in Markup.multiline_regexs:
+            pee = regex.sub(repl, pee)
+
+        pee = "".join( ["<p>%s</p>\n" % x.strip() for x in Markup.newline_split_regex.split(pee) if x.strip() != ''])
+
+        for regex, repl in Markup.oneline_regexs:
+            pee = regex.sub(repl, pee)
+        return pee
+
+
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+    import sys
+    if len(sys.argv) == 3:
+        f = open(sys.argv[1], 'r')
+        o = open(sys.argv[2], 'w')
+        raw = f.read()
+        o.write(Markup.render(raw))
+
 
